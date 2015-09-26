905c17dd52a4c04097616da40c1e7cc948d02682
iBullRay <bullray097@yandex.ru>
2015-09-26 20:33:05 +0300

    Fixed error with kernel modules and with libctest.so
    Signed-off-by: iBullRay <bullray097@yandex.ru>

--- mangov2-vendor-blobs.mk
+++ mangov2-vendor-blobs.mk
@@ -69,7 +69,6 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/system/lib/libACT_ISP.so:system/lib/libACT_ISP.so \
 	$(LOCAL_PATH)/system/lib/libalc.so:system/lib/libalc.so \
 	$(LOCAL_PATH)/system/lib/libbacklight.so:system/lib/libbacklight.so \
-	$(LOCAL_PATH)/system/lib/libctest.so:system/lib/libctest.so \
 	$(LOCAL_PATH)/system/lib/libimg_en.so:system/lib/libimg_en.so \
 	$(LOCAL_PATH)/system/lib/libion.so:system/lib/libion.so \
 	$(LOCAL_PATH)/system/lib/libsub.so:system/lib/libsub.so \
@@ -114,53 +113,53 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/system/kernel/misc/gpiocfg.xml:system/kernel/misc/gpiocfg.xml \
 	$(LOCAL_PATH)/system/kernel/misc/lpow_pic.bin:system/kernel/misc/lpow_pic.bin \
 	$(LOCAL_PATH)/system/kernel/misc/pinctrl.xml:system/kernel/misc/pinctrl.xml \
-	$(LOCAL_PATH)/system/kernel/misc/act_vibrate.ko:system/kernel/misc/act_vibrate.ko \
-	$(LOCAL_PATH)/system/kernel/misc/acts_hcd.ko:system/kernel/misc/acts_hcd.ko \
-	$(LOCAL_PATH)/system/kernel/misc/adfus.ko:system/kernel/misc/adfus.ko \
-	$(LOCAL_PATH)/system/kernel/misc/asix.ko:system/kernel/misc/asix.ko \
-	$(LOCAL_PATH)/system/kernel/misc/atc260x_adckeypad.ko:system/kernel/misc/atc260x_adckeypad.ko \
-	$(LOCAL_PATH)/system/kernel/misc/atc260x_audio.ko:system/kernel/misc/atc260x_audio.ko \
-	$(LOCAL_PATH)/system/kernel/misc/atc260x_cap_gauge.ko:system/kernel/misc/atc260x_cap_gauge.ko \
-	$(LOCAL_PATH)/system/kernel/misc/atc260x_onoff.ko:system/kernel/misc/atc260x_onoff.ko \
-	$(LOCAL_PATH)/system/kernel/misc/atc260x_power.ko:system/kernel/misc/atc260x_power.ko \
-	$(LOCAL_PATH)/system/kernel/misc/atc260x_rtc.ko:system/kernel/misc/atc260x_rtc.ko \
-	$(LOCAL_PATH)/system/kernel/misc/atv5202_backlight.ko:system/kernel/misc/atv5202_backlight.ko \
-	$(LOCAL_PATH)/system/kernel/misc/atv5202_de.ko:system/kernel/misc/atv5202_de.ko \
-	$(LOCAL_PATH)/system/kernel/misc/atv5202_fb.ko:system/kernel/misc/atv5202_fb.ko \
-	$(LOCAL_PATH)/system/kernel/misc/atv5202_lcd.ko:system/kernel/misc/atv5202_lcd.ko \
-	$(LOCAL_PATH)/system/kernel/misc/atv5202_mmc.ko:system/kernel/misc/atv5202_mmc.ko \
-	$(LOCAL_PATH)/system/kernel/misc/atv5202_vout.ko:system/kernel/misc/atv5202_vout.ko \
-	$(LOCAL_PATH)/system/kernel/misc/camera_drv.ko:system/kernel/misc/camera_drv.ko \
-	$(LOCAL_PATH)/system/kernel/misc/camera_gc0308.ko:system/kernel/misc/camera_gc0308.ko \
-	$(LOCAL_PATH)/system/kernel/misc/camera_hi253.ko:system/kernel/misc/camera_hi253.ko \
-	$(LOCAL_PATH)/system/kernel/misc/cifs.ko:system/kernel/misc/cifs.ko \
-	$(LOCAL_PATH)/system/kernel/misc/ctp_ft5x06.ko:system/kernel/misc/ctp_ft5x06.ko \
-	$(LOCAL_PATH)/system/kernel/misc/dummy_udc.ko:system/kernel/misc/dummy_udc.ko \
-	$(LOCAL_PATH)/system/kernel/misc/dwc3.ko:system/kernel/misc/dwc3.ko \
-	$(LOCAL_PATH)/system/kernel/misc/dwc3-actions.ko:system/kernel/misc/dwc3-actions.ko \
-	$(LOCAL_PATH)/system/kernel/misc/g_android.ko:system/kernel/misc/g_android.ko \
-	$(LOCAL_PATH)/system/kernel/misc/galcore.ko:system/kernel/misc/galcore.ko \
-	$(LOCAL_PATH)/system/kernel/misc/gl5202_hdmi.ko:system/kernel/misc/gl5202_hdmi.ko \
-	$(LOCAL_PATH)/system/kernel/misc/gl5202_tvout.ko:system/kernel/misc/gl5202_tvout.ko \
-	$(LOCAL_PATH)/system/kernel/misc/gsensor_mc3210.ko:system/kernel/misc/gsensor_mc3210.ko \
-	$(LOCAL_PATH)/system/kernel/misc/gspca_main.ko:system/kernel/misc/gspca_main.ko \
-	$(LOCAL_PATH)/system/kernel/misc/mcs7830.ko:system/kernel/misc/mcs7830.ko \
-	$(LOCAL_PATH)/system/kernel/misc/mmc_block.ko:system/kernel/misc/mmc_block.ko \
-	$(LOCAL_PATH)/system/kernel/misc/mmc_core.ko:system/kernel/misc/mmc_core.ko \
-	$(LOCAL_PATH)/system/kernel/misc/monitor.ko:system/kernel/misc/monitor.ko \
-	$(LOCAL_PATH)/system/kernel/misc/option.ko:system/kernel/misc/option.ko \
-	$(LOCAL_PATH)/system/kernel/misc/remotefs_inode.ko:system/kernel/misc/remotefs_inode.ko \
-	$(LOCAL_PATH)/system/kernel/misc/rtl8150.ko:system/kernel/misc/rtl8150.ko \
-	$(LOCAL_PATH)/system/kernel/misc/scsi_wait_scan.ko:system/kernel/misc/scsi_wait_scan.ko \
-	$(LOCAL_PATH)/system/kernel/misc/sndrv.ko:system/kernel/misc/sndrv.ko \
-	$(LOCAL_PATH)/system/kernel/misc/sr9700.ko:system/kernel/misc/sr9700.ko \
-	$(LOCAL_PATH)/system/kernel/misc/stub.ko:system/kernel/misc/stub.ko \
-	$(LOCAL_PATH)/system/kernel/misc/udc.ko:system/kernel/misc/udc.ko \
-	$(LOCAL_PATH)/system/kernel/misc/usb_wwan.ko:system/kernel/misc/usb_wwan.ko \
-	$(LOCAL_PATH)/system/kernel/misc/vce_drv.ko:system/kernel/misc/vce_drv.ko \
-	$(LOCAL_PATH)/system/kernel/misc/vde_drv.ko:system/kernel/misc/vde_drv.ko \
-	$(LOCAL_PATH)/system/kernel/misc/wlan.ko:system/kernel/misc/wlan.ko \
-	$(LOCAL_PATH)/system/kernel/misc/xhci-hcd.ko:system/kernel/misc/xhci-hcd.ko
+	$(LOCAL_PATH)/system/kernel/misc/modules/act_vibrate.ko:system/kernel/misc/modules/act_vibrate.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/acts_hcd.ko:system/kernel/misc/modules/acts_hcd.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/adfus.ko:system/kernel/misc/modules/adfus.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/asix.ko:system/kernel/misc/modules/asix.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/atc260x_adckeypad.ko:system/kernel/misc/modules/atc260x_adckeypad.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/atc260x_audio.ko:system/kernel/misc/modules/atc260x_audio.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/atc260x_cap_gauge.ko:system/kernel/misc/modules/atc260x_cap_gauge.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/atc260x_onoff.ko:system/kernel/misc/modules/atc260x_onoff.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/atc260x_power.ko:system/kernel/misc/modules/atc260x_power.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/atc260x_rtc.ko:system/kernel/misc/modules/atc260x_rtc.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/atv5202_backlight.ko:system/kernel/misc/modules/atv5202_backlight.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/atv5202_de.ko:system/kernel/misc/modules/atv5202_de.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/atv5202_fb.ko:system/kernel/misc/modules/atv5202_fb.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/atv5202_lcd.ko:system/kernel/misc/modules/atv5202_lcd.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/atv5202_mmc.ko:system/kernel/misc/modules/atv5202_mmc.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/atv5202_vout.ko:system/kernel/misc/modules/atv5202_vout.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/camera_drv.ko:system/kernel/misc/modules/camera_drv.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/camera_gc0308.ko:system/kernel/misc/modules/camera_gc0308.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/camera_hi253.ko:system/kernel/misc/modules/camera_hi253.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/cifs.ko:system/kernel/misc/modules/cifs.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/ctp_ft5x06.ko:system/kernel/misc/modules/ctp_ft5x06.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/dummy_udc.ko:system/kernel/misc/modules/dummy_udc.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/dwc3.ko:system/kernel/misc/modules/dwc3.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/dwc3-actions.ko:system/kernel/misc/modules/dwc3-actions.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/g_android.ko:system/kernel/misc/modules/g_android.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/galcore.ko:system/kernel/misc/modules/galcore.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/gl5202_hdmi.ko:system/kernel/misc/modules/gl5202_hdmi.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/gl5202_tvout.ko:system/kernel/misc/modules/gl5202_tvout.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/gsensor_mc3210.ko:system/kernel/misc/modules/gsensor_mc3210.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/gspca_main.ko:system/kernel/misc/modules/gspca_main.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/mcs7830.ko:system/kernel/misc/modules/mcs7830.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/mmc_block.ko:system/kernel/misc/modules/mmc_block.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/mmc_core.ko:system/kernel/misc/modules/mmc_core.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/monitor.ko:system/kernel/misc/modules/monitor.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/option.ko:system/kernel/misc/modules/option.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/remotefs_inode.ko:system/kernel/misc/modules/remotefs_inode.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/rtl8150.ko:system/kernel/misc/modules/rtl8150.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/scsi_wait_scan.ko:system/kernel/misc/modules/scsi_wait_scan.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/sndrv.ko:system/kernel/misc/modules/sndrv.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/sr9700.ko:system/kernel/misc/modules/sr9700.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/stub.ko:system/kernel/misc/modules/stub.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/udc.ko:system/kernel/misc/modules/udc.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/usb_wwan.ko:system/kernel/misc/modules/usb_wwan.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/vce_drv.ko:system/kernel/misc/modules/vce_drv.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/vde_drv.ko:system/kernel/misc/modules/vde_drv.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/wlan.ko:system/kernel/misc/modules/wlan.ko \
+	$(LOCAL_PATH)/system/kernel/misc/modules/xhci-hcd.ko:system/kernel/misc/modules/xhci-hcd.ko
 
 # OMX
 PRODUCT_COPY_FILES += \
